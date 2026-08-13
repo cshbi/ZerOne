b4888b338fe781195f911bddcef21538bde01135
서윤지 <sdbswl0602@gmail.com>
2026-08-13 14:13:07 +0900

    6주차수업시간중

--- chat_course/week06_rooms_naive/client.py
+++ chat_course/week06_rooms_naive/client.py
@@ -17,7 +17,7 @@ import threading
 from messages import TextMessage, EmojiMessage, FileMessage
 from codec import AesGcmCodec
 
-HOST = "127.0.0.1"
+HOST = "192.168.0.84"
 PORT = 5000
 CODEC = AesGcmCodec()   # 서버와 같은 codec/암호(passphrase)여야 서로 알아봄
 
